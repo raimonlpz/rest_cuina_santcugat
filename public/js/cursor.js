23ce73e55c7c0dfc6f4a6317c7dbceb00f7342a7
Raimon López <raimon@bokokode.com>
2024-07-04 17:39:55 +0200

    testing cursor for mobile

--- public/js/cursor.js
+++ public/js/cursor.js
@@ -32,7 +32,32 @@ document.addEventListener("DOMContentLoaded", function (event) {
     });
   };
 
+  window.ontouchmove = function (e) {
+    var mouseX = e.clientX;
+    var mouseY = e.clientY;
+
+    if (!initCursor) {
+      // cursor.style.opacity = 1;
+      TweenLite.to(cursor, 0.3, {
+        opacity: 1,
+      });
+      initCursor = true;
+    }
+
+    TweenLite.to(cursor, 0, {
+      top: mouseY + "px",
+      left: mouseX + "px",
+    });
+  };
+
   window.onmouseout = function (e) {
+    TweenLite.to(cursor, 0.3, {
+      opacity: 0,
+    });
+    initCursor = false;
+  };
+
+  window.ontouchout = function (e) {
     TweenLite.to(cursor, 0.3, {
       opacity: 0,
     });
